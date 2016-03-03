458949138f25841e0f875ea3c6db5c768c6dc0e5
Jesse Lieman-Sifry <jliemansifry@wesleyan.edu>
2016-03-03 13:40:24 -0700

    Refactor model beheading and addition of conv layers

--- keras_models_class.py
+++ keras_models_class.py
@@ -8,9 +8,12 @@ from keras.models import Sequential, model_from_json
 from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.normalization import BatchNormalization
 from keras.utils import np_utils
 from keras.optimizers import SGD
 import h5py
+import matplotlib # necessary to save plots remotely; comment out if local
+matplotlib.use('Agg') # comment out if local
 import matplotlib.pyplot as plt
 from keras.callbacks import EarlyStopping
 #import cv2
@@ -52,10 +55,14 @@ class keras_models(object):
     def load_data(self, data_dir, equal_classes=True, 
                   centerpix_or_pixelwise='centerpix'):
         all_image_filenames = os.listdir(data_dir)
-        satellite_filenames = [f for f in all_image_filenames 
-                               if 'satellite' in f]
-        segmented_filenames = [f for f in all_image_filenames 
-                               if 'segmented' in f]
+        satellite_filenames = sorted([f for f in all_image_filenames 
+                               if 'satellite' in f])
+        segmented_filenames = sorted([f for f in all_image_filenames 
+                               if 'segmented' in f])
+        files_lined_up = np.all([satf[:23] == segf[:23] 
+                                for satf, segf 
+                                in zip(satellite_filenames, segmented_filenames)])
+        print 'It is {} that your files are ordered correctly.'.format(files_lined_up)
         total_num_img = len(satellite_filenames)
         all_satellite_data = np.zeros((total_num_img, 
                                        self.h, 
@@ -68,10 +75,10 @@ class keras_models(object):
         print 'Loading image data...'
         for idx, satellite_filename in enumerate(satellite_filenames):
             satellite_img = imread('{}/{}'.format(data_dir, satellite_filename))
-            all_satellite_data[idx] = satellite_img
+            all_satellite_data[idx] = satellite_img[:self.h, :, :]
         for idx, segmented_filename in enumerate(segmented_filenames):
             segmented_img = imread('{}/{}'.format(data_dir, segmented_filename))
-            all_class_data_as_rgb[idx] = segmented_img
+            all_class_data_as_rgb[idx] = segmented_img[:self.h, :, :]
         h_start_pxs = np.arange(0, self.h-self.sub_im_width+1, 
                                 self.sample_stride)
         w_start_pxs = np.arange(0, self.w-self.sub_im_width+1, 
@@ -159,22 +166,25 @@ class keras_models(object):
                 classwise_pixcount_per_img = [np.sum(y[:, :, :, i]==1, axis=2).sum(1)
                                               for i in range(self.n_classes)]
                 total_pix_in_img = self.sub_im_width**2
-                min_road_pix = total_pix_in_img * 0.05
-                min_water_pix = total_pix_in_img * 0.10
-                road_img_locs = classwise_pixcount_per_img[1] > min_road_pix
-                water_img_locs = classwise_pixcount_per_img[2] > min_water_pix
-                road_and_water_locs = np.logical_and(road_img_locs,
-                                                    water_img_locs)
-                X = X[road_and_water_locs]
-                y = y[road_and_water_locs]
-                del road_and_water_locs
+                min_road_pix = total_pix_in_img * 0.02
+                min_water_pix = total_pix_in_img * 0.02
+                min_building_pix = total_pix_in_img * 0.02
+                building_img_locs = classwise_pixcount_per_img[0] > min_building_pix
+                water_img_locs = classwise_pixcount_per_img[1] > min_water_pix
+                road_img_locs = classwise_pixcount_per_img[2] > min_road_pix
+                not_just_background_locs = np.logical_or(
+                                            np.logical_or(road_img_locs,
+                                                          water_img_locs),
+                                                         building_img_locs)
+                X = X[not_just_background_locs]
+                y = y[not_just_background_locs]
+                del not_just_background_locs
                 del water_img_locs
                 del road_img_locs
         elif centerpix_or_pixelwise == 'centerpix':
             if equal_classes:
                 y_without_background = y[y != 3]
                 X_without_background = X[y != 3]
-                self.n_classes_no_background = self.n_classes - 1
                 len_smallest_class = np.min([np.sum(y_without_background==i) 
                     for i in range(self.n_classes_no_background)])
                 X_eq = np.zeros((len_smallest_class*self.n_classes_no_background,
@@ -214,59 +224,77 @@ class keras_models(object):
                                             input_shape=(self.n_chan,
                                                          self.sub_im_width,
                                                          self.sub_im_width)),
+                            # Convolution2D(self.n_conv_nodes/2,
                             Convolution2D(self.n_conv_nodes/2,
                                           self.conv_size,
                                           self.conv_size), 
-                            # LeakyReLU(alpha=0.01),
-                            Activation('relu'),
+                            LeakyReLU(alpha=0.01),
+                            # BatchNormalization(),
+                            # Activation('relu'),
                             ZeroPadding2D((1, 1)),
                             Convolution2D(self.n_conv_nodes/2,
                                           self.conv_size,
                                           self.conv_size),
-                            # LeakyReLU(alpha=0.01),
-                            Activation('relu'),
-                            MaxPooling2D(pool_size=(self.pool_size, self.pool_size)),
-                            ZeroPadding2D((1, 1)),
-                            Convolution2D(self.n_conv_nodes/2,
-                                          self.conv_size,
-                                          self.conv_size),
-                            # LeakyReLU(alpha=0.01),
-                            Activation('relu'),
+                            LeakyReLU(alpha=0.01),
+                            BatchNormalization(),
+                            # Activation('relu'),
+                            # MaxPooling2D(pool_size=(self.pool_size, self.pool_size)),
                             ZeroPadding2D((1, 1)),
                             Convolution2D(self.n_conv_nodes,
                                           self.conv_size,
                                           self.conv_size),
-                            # LeakyReLU(alpha=0.01),
-                            Activation('relu'),
-                            MaxPooling2D(pool_size=(self.pool_size, self.pool_size)),
+                            LeakyReLU(alpha=0.01),
+                            # BatchNormalization(),
+                            # Activation('relu'),
+                            ZeroPadding2D((1, 1)),
+                            Convolution2D(self.n_conv_nodes,
+                                          self.conv_size,
+                                          self.conv_size),
+                            LeakyReLU(alpha=0.01),
+                            BatchNormalization(),
+                            # Activation('relu'),
+                            # MaxPooling2D(pool_size=(self.pool_size, self.pool_size)),
                             # Dropout(self.primary_dropout),
                             Flatten(),
-                            Dense(self.n_dense_nodes*2),
+                            Dense(self.n_dense_nodes),
+                            LeakyReLU(alpha=0.01),
+                            # Activation('relu'),
+                            # Dense(self.n_dense_nodes*2),
                             # LeakyReLU(alpha=0.01),
-                            Activation('relu'),
-                            Dense(self.n_dense_nodes*2),
-                            # LeakyReLU(alpha=0.01),
-                            Activation('relu'),
+                            # Activation('relu'),
                             # Dropout(self.secondary_dropout),
                             Dense(self.n_classes_no_background),
                             Activation('softmax')]
-        if segmentation:
-            for _ in range(7):
-                model_param_to_add.pop(-1)
-            model_param_to_add += [Convolution2D(128, 1, 1)]
-            model_param_to_add += [Activation('relu')]
-            model_param_to_add += [Convolution2D(3, 1, 1)]
-            model_param_to_add += [Reshape((self.sub_im_width*self.sub_im_width, 
-                                            self.n_classes))]
-            model_param_to_add += [Activation('softmax')]
         for process in model_param_to_add:
             model.add(process)
         # sgd = SGD(lr=0.001, decay=2e-4, momentum=0.9, nesterov=True)
         model.compile(loss='categorical_crossentropy', optimizer='adadelta')
         print 'Done.'
         return model
-
     
+
+    def behead_model(self, model, num_layers_to_chop=5):
+        print 'Beheading {} layers from model...'.format(num_layers_to_chop)
+        model_layers = model.layers
+        for layer in range(num_layers_to_chop):
+            model_layers.pop(-1)
+        print 'Done.'
+        return model_layers
+
+
+    def add_pixelwise_head(self, model_layers):
+        print 'Adding convolutional layers to model...'
+        model = Sequential()
+        model_layers += [Convolution2D(self.n_classes, 1, 1)]
+        model_layers += [Reshape((self.sub_im_width*self.sub_im_width, 
+                                        self.n_classes))]
+        model_layers += [Activation('softmax')]
+        for process in model_layers:
+            model.add(process)
+        print 'Done.'
+        return model
+        
+   
     def load_model_weights(self, path_to_model, centerpix_or_pixelwise):
         ''' 
         INPUT:  (1) String: The path to the saved model architecture and weights, 
@@ -276,23 +304,26 @@ class keras_models(object):
         print 'Loading model weights...'
         json_file_name = '{}.json'.format(path_to_model)
         weights_file_name = '{}.h5'.format(path_to_model)
-        if centerpix_or_pixelwise == 'centerpix':
+        if centerpix_or_pixelwise == 'pixelwise':
             weights_file = h5py.File(weights_file_name)
-            model = self.compile_model(segmentation=True)
+            model_structure = model_from_json(open(json_file_name).read())
+            model_layers = self.behead_model(model_structure, num_layers_to_chop=5)
+            model = self.add_pixelwise_head(model_layers)
+            # model = self.compile_model(segmentation=True)
             for layer_num in range(weights_file.attrs['nb_layers']):
-                print layer_num
+                print 'Loading weights for layer {}'.format(layer_num)
                 if layer_num >= len(model.layers)-5:
                     break
                 weights_obj = weights_file['layer_{}'.format(layer_num)]
                 weights = [weights_obj['param_{}'.format(p)]
-                        for p in range(weights_obj.attrs['nb_params'])]
+                           for p in range(weights_obj.attrs['nb_params'])]
                 model.layers[layer_num].set_weights(weights)
             weights_file.close()
-        elif centerpix_or_pixelwise == 'pixelwise':
+        elif centerpix_or_pixelwise == 'centerpix':
             model = model_from_json(open(json_file_name).read())
             model.load_weights(weights_file_name)
         model.compile(loss='categorical_crossentropy', optimizer='adadelta')
-        print 'Done.'
+        print 'Done loading model weights.'
         return model
 
  
@@ -304,13 +335,13 @@ class keras_models(object):
         print 'Fitting model...\n'
         start = time.clock()
         early_stopping_monitor = EarlyStopping(monitor='val_loss', 
-                                            patience=60,
+                                            patience=2,
                                             verbose=1)
         hist = model.fit(X, y, batch_size=self.batch_size,
                         nb_epoch=self.n_epoch,
                         callbacks=[early_stopping_monitor],
                         show_accuracy=True, verbose=1,
-                        validation_split=0.1)
+                        validation_split=0.166)
         print hist.history
         stop = time.clock()
         print 'Done.'
@@ -332,29 +363,37 @@ class keras_models(object):
         return model, path_to_save_model
 
 
-def run_centerpix_defined_model(data_folder):
-    km = keras_models(n_epoch=10, sub_im_width=64, batch_size=64, n_classes=4,
-                      sample_stride=24, n_conv_nodes=128, n_dense_nodes=128)
+def run_centerpix_defined_model(data_folder, name_append):
+    km = keras_models(n_epoch=10, sub_im_width=64, batch_size=32, n_classes=4,
+                      h=620, sample_stride=20, n_conv_nodes=128, n_dense_nodes=128)
     X, y = km.load_data(data_folder, equal_classes=True,
                         centerpix_or_pixelwise='centerpix')
+    # return X, y
+    name_to_append = 'centerpix_{}'.format(name_append)
     model = km.compile_model(segmentation=False)
-    model, path_to_centerpix_model = km.fit_and_save_model(model, 'centerpix', X, y)
-    return model
+    model, path_to_centerpix_model = km.fit_and_save_model(model, name_to_append, X, y)
+    return X, y, model, path_to_centerpix_model
 
-def run_pixelwise_defined_model(data_folder):
+def run_pixelwise_defined_model(data_folder, path_to_centerpix_model, name_append):
     km = keras_models(n_epoch=10, sub_im_width=64, batch_size=64, n_classes=4,
                       sample_stride=64, n_conv_nodes=128, n_dense_nodes=128)
-    X_segmented, y_segmented = km.load_data(data_folder, equal_classes=False,
+    X_segmented, y_segmented = km.load_data(data_folder, equal_classes=True,
                                             centerpix_or_pixelwise='pixelwise')
-    segmented_model = km.compile_model(segmentation=True)
-    # segmented_model = km.load_model_weights(path_to_centerpix_model, 
-                                            # centerpix_or_pixelwise='pixelwise')
+    # segmented_model = km.compile_model(segmentation=True)
+    segmented_model = km.load_model_weights(path_to_centerpix_model, 
+                                            centerpix_or_pixelwise='pixelwise')
+    name_to_append = 'pixelwise_{}'.format(name_append)
     segmented_model, path_to_pixelwise_model = km.fit_and_save_model(segmented_model, 
-                                                                     'pixelwise',
-                                                                      X_segmented, 
-                                                                      y_segmented) 
+                                                                     name_to_append,
+                                                                     X_segmented, 
+                                                                     y_segmented) 
+    return X_segmented, y_segmented, segmented_model
     # pixelwise_prediction(segmented_model, 'data640x640zoom18/lat_28.48830,long_-81.5087_satellite.png', 'data640x640zoom18/lat_28.48830,long_-81.5087_segmented.png')
 
 if __name__ == '__main__':
-    data_folder = 'data640x640newColzoom18'
-    run_pixelwise_defined_model(data_folder)
+    data_folder = 'data640x640new2Colzoom18'
+    # name_append = '2xoversampled_nobatchnorm_32batch_c163264128d128'#_zeroinit'
+    name_append = 'oversampled_no12811_equalerclasses'
+    path_to_centerpix_model = 'models/KerasBaseModel_v.0.2_centerpix_oversampled'
+    # X, y, model, path_to_centerpix_model = run_centerpix_defined_model(data_folder, name_append)
+    X_seg, y_seg, seg_model = run_pixelwise_defined_model(data_folder, path_to_centerpix_model, name_append)
